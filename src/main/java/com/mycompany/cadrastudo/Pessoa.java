784c1f7ea778c034a3650b03756ae70aa42fff10
souajenni <jennifervialli1@gmail.com>
2024-07-24 12:57:55 -0300

    Base

---
+++ src/main/java/com/mycompany/cadrastudo/Pessoa.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cadrastudo;
+
+/**
+ *
+ * @author Daniel Dória
+ */
+public class Pessoa {
+    private String nome;
+    private String email;
+    private String usuario;
+    private String senha;
+    private int nascimento;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public int getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(int nascimento) {
+        this.nascimento = nascimento;
+    }   
+}
+
